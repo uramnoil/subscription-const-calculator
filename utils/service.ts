6d986eebb25dd443d0319be0b5545a497fb95f94
uramnoil <25810983+UramnOIL@users.noreply.github.com>
2024-05-15 00:23:05 +0900

    :sparkles: add Spotify prices

--- utils/service.ts
+++ utils/service.ts
@@ -8,7 +8,18 @@ interface Plan {
   price: number;
 }
 
-const allServices: Service[] = []; 
+const allServices: Service[] = [
+  {
+    name: "Spotify",
+    plans: [
+      { name: "Free", price: 0 },
+      { name: "Standard", price: 980 },
+      { name: "Student", price: 480 },
+      { name: "Duo", price: 1280 },
+      { name: "Family", price: 1580 },
+    ],
+  }
+]; 
 
 export type { Plan, Service };
 export { allServices };
